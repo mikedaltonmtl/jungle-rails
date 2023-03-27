46a099b74f71eeddbb04430ff8671681438af365
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-03-27 18:23:01 +0000

    add Cypress tests for product page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('jungle rails app', () => {
+  beforeEach(() => {
+    cy.visit('http://0.0.0.0:3000');
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("When clicking on a product, the product detail page is visible", () => {
+    // The same product that was clicked on should be displayed
+    cy.get('[alt="Scented Blade"]').click();
+    cy.get('[alt="Scented Blade"]').should("be.visible");
+    // There should only be one product displayed
+    cy.get("article.product-detail").should("have.length", 1);
+  });
+
+});
